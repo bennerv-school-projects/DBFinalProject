643792a8dce7c00cae63c768f64eded18029f78d
bennerv <bves94@gmail.com>
2016-12-03 13:48:25 -0500

    Update stored_procecures.sql

--- stored_procecures.sql
+++ stored_procecures.sql
@@ -3,6 +3,7 @@ delimiter //
 
 DROP PROCEDURE IF EXISTS create_exam;
 DROP PROCEDURE IF EXISTS create_question;
+DROP PROCEDURE IF EXISTS create_choice;
 
 
 # Create procedure for the exam table to create a new exam
@@ -36,6 +37,36 @@ BEGIN
       if CHAR_LENGTH(question_contents) > 0 THEN
         if questionNumber = 0 THEN
           insert into question values( exam_name, question_number, answer, points, question_contents);
+        end if;
+      end if;
+    end if;
+  end if;
+end//
+
+# Create a procedure for the choice table to create a choice for an exam question
+CREATE PROCEDURE create_choice(
+  in id char(1),
+  in exam_name varchar(50),
+  in question_number integer,
+  in choice_contents varchar(256)
+)
+
+BEGIN
+  declare numberOfExams integer;
+  declare questionNumber integer;
+  delcare idNumber integer;
+  
+  select COUNT(*) into numberOfExams FROM exam WHERE exam.exam_name=exam_name;
+  select COUNT(*) into questionNumber FROM question WHERE question.exam_name=exam_name AND question.question_number=question_number;
+  select COUNT(*) into idNumber FROM choice WHERE choice.id=id;
+  
+  if numberOfExams = 1 THEN
+    if questionNumber = 1 THEN 
+      if CHAR_LENGTH(choice_contents) > 0 THEN
+        if CHAR_LENGTH(id) > 0 THEN
+          if idNumber = 0 THEN
+            insert into choice values(id, exam_name, question_number, choice_contents);
+          end if;
         end if;
       end if;
     end if;
