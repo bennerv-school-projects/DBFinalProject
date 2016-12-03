6ad1babb2c3cbd94036139100f9442aa0ae07b82
bennerv <bmvesel@mtu.edu>
2016-12-03 14:34:36 -0500

    Added in initial signin functionality

---
+++ signin.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+
+?>
+
+
+
+<html>
+	<body>
+		<form action="" method="post>
+			Student ID: <input type="text" name="username"/> <br/>
+			Password: <input type="password" name="password"/> <br/>
+			<input type="submit" name="loginButton" value="Log In"/>
+		</form>
+
+		<a href="signup.php">Sign up</a>
+
+
+
+
+
+
+
+	</body>
+</html>
