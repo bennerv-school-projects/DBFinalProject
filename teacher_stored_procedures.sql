1e0e93221cf1a2abc6a1f0189be3fde46d3e784b
bennerv <bmvesel@mtu.edu>
2016-12-10 04:31:21 -0500

    Fixed the question stored procedure to make it so it inserts ascending question numbers

--- teacher_stored_procedures.sql
+++ teacher_stored_procedures.sql
@@ -19,7 +19,6 @@ end//
 # Create procedure for the question table to create a question for an exam
 CREATE PROCEDURE create_question(
   in exam_name varchar(50),
-  in question_number integer,
   in answer char(1),
   in points integer,
   in question_contents varchar(256)
@@ -30,14 +29,12 @@ BEGIN
   declare questionNumber integer;
   
   select COUNT(*) into numOfExams FROM exam where exam.exam_name=exam_name;
-  select COUNT(*) into questionNumber FROM question where question.exam_name=exam_name AND question.question_number=question_number;
+  select 1 + COUNT(question_number) into questionNumber FROM question where question.exam_name=exam_name;
   
   if numOfExams = 1 THEN
     if CHAR_LENGTH(answer) = 1 THEN
       if CHAR_LENGTH(question_contents) > 0 THEN
-        if questionNumber = 0 THEN
-          insert into question values( exam_name, question_number, answer, points, question_contents);
-        end if;
+        insert into question values( exam_name, question_number, answer, points, question_contents);
       end if;
     end if;
   end if;
